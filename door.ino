42c4557df3d3a7262fb507d69cf32fc50771c7fd
SkidFace <74504657+SkidFace@users.noreply.github.com>
2021-03-29 12:38:07 -0700

    Added Comments explaining what each line does

--- door.ino
+++ door.ino
@@ -1,4 +1,4 @@
-#include <Servo.h> //imports the servo library
+#include <Servo.h>	 //imports the servo library to allow servos to be used
 const int buttonPin = A0;      //these next lines define which pin is which and variables we need
 int servoPin = 0;
 int lastButtonState;    
@@ -8,7 +8,7 @@ Servo Servo1;
 
 void setup() {
   pinMode(buttonPin, INPUT);
-  currentButtonState = digitalRead(buttonPin); //sets the state of the button whether its on or off
+  currentButtonState = digitalRead(buttonPin); 		//sets the state of the button whether its on or off
   Servo1.attach(servoPin);	//tell the servo where it is
   Servo1.write(105); //the next few lines are in to make sure the servo works
   delay(1000);
@@ -23,7 +23,7 @@ void loop() {
   currentButtonState = digitalRead(buttonPin); 
   
 
-  if(lastButtonState == HIGH && currentButtonState == LOW) {  //If you want this to be used once add to the if statement: && servoUsed
+  if(lastButtonState == HIGH && currentButtonState == LOW) {  //If you want this to be used once, add this to the If statement: && servoUsed
 	//if you need delay after the door opens, add 	delay(3000);
     Servo1.write(180);	//open the servo
     delay(1000);	//adds 1 second of delay 
