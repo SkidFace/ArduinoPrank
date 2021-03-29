e5b2d6a977ff47d4a2bf11e6598802a3df3c77dc
SkidFace <74504657+SkidFace@users.noreply.github.com>
2021-03-29 09:56:12 -0700

    Update button.ino

--- button.ino
+++ button.ino
@@ -1,6 +1,6 @@
-#include <Servo.h>
-const int buttonPin = A0;      
-int servoPin = 1;
+#include <Servo.h> //imports the servo library
+const int buttonPin = A0;      //these next lines define which pin is which and variables we need
+int servoPin = 0;
 int lastButtonState;    
 int currentButtonState;
 bool servoUsed = true;
@@ -8,9 +8,9 @@ Servo Servo1;
 
 void setup() {
   pinMode(buttonPin, INPUT);
-  currentButtonState = digitalRead(buttonPin);
-  Servo1.attach(servoPin);
-  Servo1.write(105);
+  currentButtonState = digitalRead(buttonPin); //sets the state of the button whether its on or off
+  Servo1.attach(servoPin);	//tell the servo where it is
+  Servo1.write(105); //the next few lines are in to make sure the servo works
   delay(1000);
   Servo1.write(110);
   delay(1000);
@@ -19,16 +19,16 @@ void setup() {
 
 void loop() {
 
-  lastButtonState    = currentButtonState;      
+  lastButtonState    = currentButtonState;      //These lines make the switch toggle
   currentButtonState = digitalRead(buttonPin); 
   
 
-  if(lastButtonState == HIGH && currentButtonState == LOW) {  // && servoUsed
- //   delay(3000);
-    Servo1.write(180);
-    delay(1000);
-    Servo1.write(105);
-    servoUsed= false;
+  if(lastButtonState == HIGH && currentButtonState == LOW) {  //If you want this to be used once add to the if statement: && servoUsed
+	//if you need delay after the door opens, add 	delay(3000);
+    Servo1.write(180);	//open the servo
+    delay(1000);	//adds 1 second of delay 
+    Servo1.write(105);	//close the servo
+    servoUsed= false;	//tells the Arduino the servo has been used
     
    
 }
